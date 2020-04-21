71a94512d3bb9a4b85108d2ad063ec8581dfca77
JuliaDudar <44841437+JuliaDudar@users.noreply.github.com>
2020-04-21 13:24:15 +0200

    Update

--- Python-Scripts/tei_header.py
+++ Python-Scripts/tei_header.py
@@ -3,8 +3,14 @@ from bs4 import BeautifulSoup
 import xmlformatter
 import os.path
 import glob
+import string
 
-'''this script adds the tei-header, sets the xml-id, formats xml and deletes unnecesary elements'''
+
+'''this script adds the tei-header, sets the xml-id, number of words and size, formats xml,'''
+
+
+path = 'C:/Users/yulya/PycharmProjects/TEI-XML/texts/*.txt'
+save_path = 'C:/Users/yulya/PycharmProjects/TEI-XML/workxmls_formatted/'
 
 
 #read files
@@ -14,31 +20,60 @@ def line_reader(document):
     return inhalt
 
 
+#count words in each document and save results as a dictionary
+def count_words(path):
+    dict = {}
+    for file in glob.glob(path):
+        text = open(file, 'r', encoding='utf8')
+        text = text.read()
+        #delete punctuation
+        translator = str.maketrans('', '', string.punctuation)
+        words= text.translate(translator)
+        count = len(words.split())
+        name = os.path.basename(file).replace('.txt','')
+        dict[name] = count
+    return dict
+
+
+def header(file,dict,name):
+    #converting to Beautifulsoup_object
+    soup = BeautifulSoup(file, "xml")
+    #navigation to TEI-element
+    tag = soup.TEI
+    # insert xml_id from document_ name
+    tag['xml:id'] = name.replace('.xml', '')
+    #
+    for key in dict.keys():
+        #filling out the number of words from previous funktion
+        if key == name.replace('.xml', ''):
+            tag2 = soup.extent.measure
+            tag2.string.replace_with(str(dict[key]))
+            #filling out information about size
+            tag3 = soup.size
+            if dict[key] < 50000:
+                tag3['key'] = 'short'
+            elif dict[key] > 50000 and dict[key] < 100000:
+                tag3['key'] = 'medium'
+            else:
+                tag3['key'] = 'long'
+            return soup
+
 
 
 def main():
-    for file in glob.glob('C:/Users/yulya/PycharmProjects/TEI-XML/xmls_for_plain3/*.xml'):
+    dict = count_words(path)
+    for file in glob.glob('C:/Users/yulya/PycharmProjects/TEI-XML/xmls_with_div/*.xml'):
         doc1 = line_reader('teiHeader-Template.xml')
         doc2 = line_reader(file)
-        #delete linebreaks
-        doc2 = doc2.replace('\n',' ')
-        #delete unnecesary elements
-        doc2 = doc2.replace('<O/>','')
-        doc2 = doc2.replace('<C/>','')
         #combine header with text-body
         result = doc1 + doc2 + '</TEI>'
         #format
-        formatter = xmlformatter.Formatter(encoding_input="utf-8",encoding_output="utf-8")
-        formatted = formatter.format_string(result)
-        #converting to Beautifulsoup_object
-        soup = BeautifulSoup(formatted, "xml")
+        #formatter = xmlformatter.Formatter(encoding_input="utf-8",encoding_output="utf-8")
+        #formatted = formatter.format_string(result)
         #set file_name and save path
-        save_path = 'C:/Users/yulya/PycharmProjects/TEI-XML/worksxmls_formatted/'
         name = os.path.basename(file)
         fullname = os.path.join(save_path, name)
-        #insert xml_id from document_ name
-        tag = soup.TEI
-        tag['xml:id'] = name.replace('.xml','')
+        soup = header(result,dict,name)
         #save results
         fa = open(fullname, 'w', encoding="utf8")
         fa.write(str(soup))
