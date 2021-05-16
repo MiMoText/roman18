d389b4a463148adb719aa915cca33ff6839e2f0b
sarahondraszek <s1saondr@uni-trier.de>
2021-05-16 19:28:46 +0200

    NER correction

--- NER-with-SpaCy/scripts/NER_plaintexts.py
+++ NER-with-SpaCy/scripts/NER_plaintexts.py
@@ -4,34 +4,31 @@ import spacy
 import spacy.cli
 import glob
 import csv
+import pandas as pd
 
-""" Medium model for better results """
+# spacy.cli.download('fr_core_news_sm')
 
-nlp = spacy.load('fr_core_news_md', exclude=['tagger', 'parser', 'lemmatizer', 'textcat'])
 
-""" List of all files in our folder """
+nlp = spacy.load('fr_core_news_sm', exclude=['tagger', 'parser', 'lemmatizer', 'textcat'])
+
 
 file_list = glob.glob("../../plain/files/*.txt")
-# print(file_list)
-
-""" Max_length because the corpus is quite large """
-
 nlp.max_length = 2000000
-
-""" Dictionary including names of the plaintexts and the 5 most frequent LOC NE """
 
 text_dict = {}
 for file_path in file_list:
-    print('Working on:', file_path.replace('../../plain/files/', ''))
+    print('Working on:', file_path)
     perm_file = open(file_path, 'r')
     perm_text = perm_file.read()
     perm_file.close()
     perm_doc = nlp(perm_text)
     most_freq_loc = nltk.FreqDist([(ent.text, ent.label_) for ent in perm_doc.ents if ent.label_ == 'LOC']).most_common(5)
-    text_dict[file_path.replace('../../plain/files/', '')] = most_freq_loc
+    most_freq_per = nltk.FreqDist([(ent.text, ent.label_) for ent in perm_doc.ents if ent.label_ == 'PER']).most_common(5)
+    text_dict[file_path.replace('/Users/sarahreb/Downloads/roman18/plain/files/', '')] = most_freq_loc, most_freq_per
+
+""" Write data to .csv table"""
+dataframe = pd.DataFrame.from_dict(text_dict, orient='index', columns=['LOC', 'PER']).to_csv('ner_loc.csv')
 
 
-with open('ner_loc.csv', 'w', newline='') as file_output:
-    tsv_output = csv.writer(file_output, delimiter='\t')
-    tsv_output.writerow(text_dict.values())
-    tsv_output.writerow(text_dict.keys())
+
+
