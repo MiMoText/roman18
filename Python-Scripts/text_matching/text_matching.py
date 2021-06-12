a09edb86d6198b1f432c7b05b0fa85ae118d0eb9
JoKons <johanna.konstanciak@gmx.de>
2021-06-12 17:46:45 +0200

    added new frantext files

--- Python-Scripts/text_matching/text_matching.py
+++ Python-Scripts/text_matching/text_matching.py
@@ -30,14 +30,15 @@ def find_matches(text_dict, search_text_name, search_text):
 	for i, val in text_dict.items():
 		if i != search_text_name:
 			matchtext = Text(val, i)
-			numMatches, locA, locB = Matcher(txt, matchtext).match() 
+			numMatches, locA, locB = Matcher(txt, matchtext, threshold=15).match() 
 			matches_list[i] = [numMatches, locA, locB]
+			#print(type(matches_list), matches_list)
 	return matches_list
 
 def main():
 	
 	path = join("..", "..", "plain", "files", "*.txt")
-	
+	#path = join("..", "..", "plain", "matchingtestfiles", "*.txt")
 	if not os.path.exists(join("..", "..", "text_matches_csv_files")):
 		os.mkdir(join("..", "..", "text_matches_csv_files"))
 		
@@ -45,8 +46,10 @@ def main():
 	
 	
 	text_dict = {}
+	#for infile in glob.glob(path):
 	for infile in glob.glob(path):
 		filename = basename(infile).split(".")[0]
+		print(filename)
 		# read file and add to dictionary: filenames as key, texts as value
 		txt = read_file(infile, filename)
 		text_dict[filename] = txt
@@ -56,29 +59,44 @@ def main():
 		
 		# take key as index to compare it with all other texts
 		search_text_name = i
+		print(search_text_name)
 		search_text = val
 		# search for the matches, it returns a dictionary with all matches (also the 0 matches)
 		matches = find_matches(text_dict, search_text_name, search_text)
-		#df = pd.DataFrame(matches, index=["numMatches","Locations in A", "Locations in B"]).T
+		
 		
 		# create new dictionary to save all real matches and get the text-parts from Text A (i.e. key text) and Text B (i.e. one per one of all other texts)
 		match_dict = {}
 		for i1, val1 in matches.items():
+			#print("i1, val1", i1, val1)
 			if val1[0] > 0: # to get only texts where matches are found
+				#print("vals", val1[0], val1[2])
+				matches_in_search_text = val1[1]
+				matches_in_matching_text = val1[2]
+				#print(matches_in_search_text, matches_in_matching_text)
 				# get textpart in A:
-				for i, match in enumerate(val1[1]):
+				for i, match in enumerate(matches_in_search_text):
+					#print("match", i, match)
+					#print("match text 1",val1[1])
 					textA = search_text[match[0]-30:match[0]] + search_text[match[0]:match[1]].upper() + search_text[match[1]:match[1]+30]
+					
 					# get textpart in B:
-					b1 = val1[2][i][0]
-					b2 = val1[2][i][1]
+					
+					#print("match text 2", val1[2])
+					b1 = matches_in_matching_text[i][0]
+					#print("b1", b1)
+					b2 = matches_in_matching_text[i][1]
 					textB = text_dict[i1][b1-30:b1] + text_dict[i1][b1:b2].upper() + text_dict[i1][b2:b2+30]
 					# add both text parts and the locations in dictionary
-					match_dict["{}_{}".format(i1, i)] = [textA, textB, match, val1[2][0]]
-					print(val1[2][0])
-		# create dataframe from dictionary and save it as csv --> for each text there will be a csv-document
-		df_texts = pd.DataFrame(match_dict, index =["Text in {}".format(search_text_name), "Text in B", "Location in {}".format(search_text_name), "Location in B"]).T
+					match_dict["{}_{}".format(i1, i)] = [textA, textB, match, (b1, b2)]
+					#print(val1[2][0])
 		
+		#print(match_dict)
+		# create dataframe from dictionary and save it as csv --> for each text there will be a csv-document
+		if match_dict:
+			df_texts = pd.DataFrame(match_dict, index =["Text in {}".format(search_text_name), "Text in B", "Location in {}".format(search_text_name), "Location in B"]).T
+			#print(df_texts)
 
-		df_texts.to_csv(join(savepath, "{}_texts.csv".format(search_text_name)), encoding="utf8")
-		#df.to_csv("{}.csv".format(search_text_name), encoding="utf8")
+			df_texts.to_csv(join(savepath, "thresh_15_{}_texts.csv".format(search_text_name)), encoding="utf8")
+		
 main()
