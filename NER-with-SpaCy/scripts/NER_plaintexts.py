4f52bbe6e29bfc896d0dc29b24fa4592b17869fb
Henning Gebhard <henning.gebhard@rub.de>
2021-11-24 18:46:27 +0100

    NER_plaintext.py now accepts command line parameters
    All the config variables available at the top of the script
    can now be overridden on a per-use basis with command line
    parameters. For a list of options have a look at the README
    or use `python NER_plaintexts.py --help`.

--- NER-with-SpaCy/scripts/NER_plaintexts.py
+++ NER-with-SpaCy/scripts/NER_plaintexts.py
@@ -7,6 +7,7 @@ Parse a directory of plain text files, find named entities
 with spaCy, and store the results in a .csv table.
 '''
 
+import argparse
 from collections import Counter
 from functools import reduce
 from glob import glob
@@ -19,8 +20,8 @@ import spacy
 # Adjust the following config variables as needed.
 # The directory where the plain text files are stored, and where
 # the eventual results should be stored.
-PLAIN_TEXT_DIR = '../../plain/files/'
-RESULT_FILE_PATH = 'ner_loc_per.csv'
+SOURCES_PATH = '../../plain/files/'
+RESULTS_PATH = 'ner_loc_per.csv'
 
 # On machines with limited memory, some of the larger
 # documents might cause OOM. To prevent that, it is
@@ -32,7 +33,7 @@ CHUNK_SIZE = 120_000
 # at document creation to prevent OOM. The default
 # is 1_000_000. If `CHUNK_SIZE` is used, it will probably
 # prevent the docs from ever reaching this size.
-MAX_DOC_LENGTH = 2_000_000 
+MAX_DOC_LENGTH = 2_800_000 
 
 # Reload the spaCy language model every N iterations.
 # Drastically increases runtime, but can save a bit of
@@ -47,14 +48,14 @@ MOST_FREQUENT_COUNT = 5
 
 
 
-def get_nlp_loader(iterations=RELOAD_MODEL):
+def get_nlp_loader(iterations=RELOAD_MODEL, doc_length=MAX_DOC_LENGTH):
     '''Memoize and periodically reload the language model to clear the vocab and string store memory.
     Drastically increases runtime, but can save memory in case spaCy's own String Store gets to large.
     '''
     # The larger, but more accurate pre-trained french language model has to be installed with
     # `python -m spacy download fr_core_news_lg`
     nlp = spacy.load("fr_core_news_lg", exclude=['tagger', 'parser', 'lemmatizer', 'textcat'])
-    nlp.max_length = 2_000_000
+    nlp.max_length = doc_length
     tokenizer = nlp.tokenizer
     it = 1
 
@@ -66,7 +67,7 @@ def get_nlp_loader(iterations=RELOAD_MODEL):
         if iterations is not None and it % iterations == 0:
             logging.warning('reloading language model')
             nlp = spacy.load("fr_core_news_lg", exclude=['tagger', 'parser', 'lemmatizer', 'textcat'])
-            nlp.max_length = 2_000_000
+            nlp.max_length = doc_length
             tokenizer = nlp.tokenizer
         else:
             logging.info('reuse language model')
@@ -81,7 +82,7 @@ def get_nlp_loader(iterations=RELOAD_MODEL):
 def get_texts(pattern):
     '''Generator which yields all the file paths matching a given glob pattern.'''
     for path in glob(pattern):
-        with open(path) as f:
+        with open(path, encoding='utf8') as f:
             text = f.read()
         name = Path(path).name
         yield (name, text) 
@@ -114,18 +115,32 @@ def sum_up_counters(counter_list):
     return reduce(lambda a, b: a+b, counter_list)
 
 
-def main():
-    results = {}
-    nlp_loader = get_nlp_loader()
+def __nullable_number(string):
+    '''Utility function used by the CLI argument parser.
+    Allows positive numbers as `ints` and returns `None`
+    in any other case.
+    '''
+    try:
+        if not string or string == 'None' or int(string) <= 0:
+            return None
+        return int(string)
+    except:
+        return None
 
-    for name, text in get_texts(f'{PLAIN_TEXT_DIR}*.txt'):
+
+def main(args):
+    results = {}
+    nlp_loader = get_nlp_loader(iterations=args.reload_model, doc_length=args.max_doc_length)
+    sources = args.sources_path if args.sources_path.endswith('.txt') else f'{args.sources_path}*.txt'
+
+    for name, text in get_texts(sources):
         logging.warning(f'Working on text {name}')
         _, tokenizer = nlp_loader()
 
         # Keep track of the most frequent NEs of each chunk.
         loc_counters = []
         per_counters = []
-        for chunk in chunk_text(text, tokenizer):
+        for chunk in chunk_text(text, tokenizer, args.chunk_size):
             logging.debug('got chunk')
             logging.debug(len(chunk))
             nlp, _ = nlp_loader()
@@ -140,13 +155,55 @@ def main():
         most_freq_loc = sum_up_counters(loc_counters)
         most_freq_per = sum_up_counters(per_counters)
         results[name] = (
-            most_freq_loc.most_common(MOST_FREQUENT_COUNT),
-            most_freq_per.most_common(MOST_FREQUENT_COUNT))
+            most_freq_loc.most_common(args.most_frequent_count),
+            most_freq_per.most_common(args.most_frequent_count))
         
     # Organize data as pandas DataFrame and write to csv file.
     df = pd.DataFrame.from_dict(results, orient='index', columns=['LOC', 'PER'])
-    df.to_csv(RESULT_FILE_PATH)
+    df.to_csv(args.results_path)
 
 
 if __name__ == '__main__':
-    main()
+    arg_parser = argparse.ArgumentParser(
+        description='Use SpaCy to get the most frequently used NEs in french novels.')
+    arg_parser.add_argument(
+        '-s', '--sources-path',
+        metavar='SOURCES_PATH',
+        default=SOURCES_PATH,
+        help='path to the directory where the plain text input files are, or to one specific txt file')
+    arg_parser.add_argument(
+        '-r' ,'--results-path',
+        metavar='RESULTS_PATH',
+        default=RESULTS_PATH,
+        help='path of .csv file where the results will be written to')
+    arg_parser.add_argument(
+        '--chunk-size',
+        type=__nullable_number,
+        metavar='WORDCOUNT',
+        default=CHUNK_SIZE,
+        help='Optionally split longer texts into chunks with this word count to reduce memory usage.')
+    arg_parser.add_argument(
+        '--max-doc-length',
+        type=int,
+        metavar='CHARCOUNT',
+        default=MAX_DOC_LENGTH,
+        help='Maximum length of documents (in characters) which SpaCy will accept')
+    arg_parser.add_argument(
+        '--reload-model',
+        type=__nullable_number,
+        default=RELOAD_MODEL,
+        metavar='I',
+        help='Optionally reload spaCy language model every I iterations.'
+        )
+    arg_parser.add_argument(
+        '-c', '--most-frequent-count',
+        default=MOST_FREQUENT_COUNT,
+        metavar='COUNT',
+        type=int,
+        help='the number of most frequent NEs to store'
+        )
+
+    args = arg_parser.parse_args()
+    logging.info('running with the following options')
+    logging.info(args)
+    main(args)
