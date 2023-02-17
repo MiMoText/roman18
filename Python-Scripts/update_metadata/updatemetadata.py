13989f0349eec7277b52a16cd5a7915dd267c896
JoKons <johanna.konstanciak@gmx.de>
2023-02-17 12:55:30 +0100

    cleaned script

--- Python-Scripts/update_metadata/updatemetadata.py
+++ Python-Scripts/update_metadata/updatemetadata.py
@@ -2,17 +2,12 @@ import glob
 import os.path
 import pandas as pd
 from os.path import join
-from lxml import etree
-import numpy as np
 from bs4 import BeautifulSoup
-import logging
 import re
 
 filepath = join("", "..", "..", "XML-TEI" , "files", "*.xml")
-#filepath = join("", "files", "*.xml")
 metadata_path = join("", "xml-tei_full_metadata_update.tsv")
 save_path = join("", "..", "..", "XML-TEI" , "files")
-#save_path = join("", "outputtest")
 
 def read_table(metadata_path):
 
@@ -36,7 +31,7 @@ def compare_title(filemetadata, xml_file):
     metatitle = metatitle.split(": MiMoText")[0]
 
     xmltitle = titleStmt.title
-    """
+    
     try:
         if xmltitle.string != metatitle:
             xmltitle.string = metatitle + ": MiMoText edition"
@@ -44,8 +39,6 @@ def compare_title(filemetadata, xml_file):
         new_tag = xml_file.new_tag("title", ref="bgrf:0 wikidata:0")
         new_tag.string = filemetadata["title"] + ": MiMoText edition"
         xml_file.titleStmt.insert(1, new_tag)
-        xmltitle = new_tag
-    """
     
     # find bgrf and wikidata
     xmlbgrf = xmltitle["ref"].split(" ")[0].split(":")[1]
@@ -70,12 +63,6 @@ def compare_author(filemetadata, xml_file):
     xmlauthor = titleStmt.author
 
     if not re.search(",", filemetadata["au-name"]):
-        """        metaauthor = filemetadata["au-name"] + "(" + str(filemetadata["au-birth"]) + "-" + str(filemetadata["au-death"]) + ")"
-        if xmlauthor.string != metaauthor:
-            xmlauthor.string = metaauthor
-        """
-
-
         xmlauthorref = xmlauthor["ref"].split(" ")
         print(xmlauthorref)
         try:
@@ -88,6 +75,8 @@ def compare_author(filemetadata, xml_file):
         
         if xmlauthorwd != filemetadata["author_wikidata"]:
             xmlauthor["ref"] = xmlauthorref[0] + " wikidata:"+ str(filemetadata["author_wikidata"])
+
+
     if re.search(",", filemetadata["au-name"]):
         print(xmlauthor)
         meta_authors = filemetadata["au-name"].split(",")
@@ -132,24 +121,13 @@ def compare_respStmts(filemetadata, xml_file):
             else:
                 all_resps_meta = [filemetadata["resp_encoding"]]
 
-            print(res.resp.find_next_siblings())
             all_resps = res.resp.find_next_siblings()
-            #all_resps_meta = filemetadata["resp_encoding"].split(",")
 
             for ind, respm in enumerate(all_resps_meta):
-                #for ind1, sib in enumerate(all_resps):
-                #print("pprint", all_resps[ind], resp)
-                print(respm, all_resps)
                 try:
                     if all_resps[ind] != respm.strip():
                         all_resps[ind].string = respm.strip()
-                        print("try", all_resps[ind])
-                    #if sib.string != resp.strip():
-                    #    sib.string = resp.strip()
-                        #print(all_resps[ind])
                 except IndexError:
-                    #if all_resps[ind].is_empty_element:
-                        #del all_resps[ind]
                     new_tag = xml_file.new_tag("name")
                     new_tag.string = respm.strip()
                     xml_file.respStmt.next_sibling.next_sibling.insert(ind+3, new_tag)
@@ -157,7 +135,7 @@ def compare_respStmts(filemetadata, xml_file):
 
 def compare_extent(filemetadata, xml_file):
 
-        # find extent
+    # find extent
     extent = xml_file.find("extent")
     words = xml_file.find("measure", {"unit":"words"})
     if words.string != filemetadata["token count"]:
@@ -203,25 +181,14 @@ def compare_digSource(filemetadata, xml_file):
         try:
             if all_digrefs[ind]["target"] != ref:
                 all_digrefs[ind]["target"] = ref
-                # all_digrefs[ind].clear()
             counter += 1
         except IndexError:
             new_tag = xml_file.new_tag("ref")
             new_tag["target"] = str(ref)
             xml_file.bibl.insert(counter, new_tag)
             counter += 1
-    """
-    try:
-        if digitalSource.ref["target"] != filemetadata['digitalSource_Ref']:
-            digitalSource.ref["target"] = filemetadata['digitalSource_Ref']
-    except AttributeError:
-        new_tag = xml_file.new_tag("ref")
-        new_tag["target"] = filemetadata['digitalSource_Ref']
-        xml_file.bibl.insert(3, new_tag)
-    """
 
     counterpubs = counter
-    #print(counterpubs)
     ## publisher
     if re.search(",", filemetadata['digitalSource_Publisher']):
         meta_all_digpubs = filemetadata['digitalSource_Publisher'].split(",")
@@ -234,26 +201,14 @@ def compare_digSource(filemetadata, xml_file):
             if all_digpubs[ind].string != pub:
                 all_digpubs[ind].string = pub
             counterpubs += 1
-            #print(counterpubs)
         except IndexError:
             new_tag = xml_file.new_tag("publisher")
             new_tag.string = str(pub)
             xml_file.bibl.insert(counterpubs, new_tag)
             counterpubs += 1
-            #print(counterpubs)
 
 
-    """
-    try:
-        if digitalSource.publisher.string != filemetadata['digitalSource_Publisher']:
-            digitalSource.publisher.string = filemetadata['digitalSource_Publisher']
-    except AttributeError:
-        new_tag = xml_file.new_tag("publisher")
-        new_tag.string = str(filemetadata["digitalSource_Publisher"])
-        xml_file.bibl.insert(2, new_tag)
-    """
     ## date
-    print("date start : ", counterpubs, counter+1)
     if re.search(",", filemetadata['digitalSource_Date']):
         meta_all_digdates = filemetadata['digitalSource_Date'].split(",")
     else:
@@ -268,22 +223,11 @@ def compare_digSource(filemetadata, xml_file):
             
             xml_file.bibl.insert(counter+1, all_digdates[ind])
             counter += 2
-            print(counterpubs)
         except IndexError:
             new_tag = xml_file.new_tag("date")
             new_tag.string = str(date)
             xml_file.bibl.insert(counter+1, new_tag)
             counter += 2
-            print(counter)
-    """
-    try:
-        if digitalSource.date.string != filemetadata['digitalSource_Date']:
-            digitalSource.date.string = filemetadata['digitalSource_Date']
-    except AttributeError:
-        new_tag = xml_file.new_tag("date")
-        new_tag.string = str(filemetadata["digitalSource_Date"])
-        xml_file.bibl.insert(4, new_tag)
-    """  
 
 
 
@@ -294,7 +238,6 @@ def compare_printSource(filemetadata, xml_file):
     # sourceDesc: printSource:
     printSource = xml_file.find("bibl", {"type": "printSource"})
     
-    """
     ## title
     try:
         if printSource.title.string != filemetadata["printSource_title"]:
@@ -303,11 +246,7 @@ def compare_printSource(filemetadata, xml_file):
         new_tag = xml_file.new_tag("title")
         new_tag.string = str(filemetadata["printSource_title"])
         xml_file.bibl.insert(1, new_tag)
-        """
-    """
-    if printSource.author.string != filemetadata['printSource_author']:
-        printSource.author.string = filemetadata['printSource_author']
-    """
+
     
     if re.search(",", filemetadata['printSource_author']):
         meta_all_printauths = filemetadata['printSource_author'].split(",")
@@ -344,35 +283,17 @@ def compare_printSource(filemetadata, xml_file):
 
     all_pubplaces = printSource.find_all("pubPlace")
     for ind, pub in enumerate(meta_all_pubplaces):
-        print("pub counter: ",pub, counter)
-        print(printSource)
         try:
             if all_pubplaces[ind].string != pub:
-                print(all_pubplaces, pub)
                 all_pubplaces[ind].string = pub
             printSource.insert(counter+1, all_pubplaces[ind])
-            print(printSource)
             counter += 1
         except IndexError:
             new_tag = xml_file.new_tag("pubPlace")
             new_tag.string = str(pub)
-            print(new_tag)
             printSource.insert(counter+1, new_tag)
             counter += 1
-    """
 
-    ## here only one String is taken into account
-    try:
-        if printSource.pubPlace.string != filemetadata['printSource_pubPlace']:
-            printSource.pubPlace.string = filemetadata['printSource_pubPlace']
-    except AttributeError:
-        new_tag = xml_file.new_tag("pubplace")
-        new_tag.string = str(filemetadata['printSource_pubPlace'])
-        xml_file.bibl.insert(1, new_tag)
-    
-    if printSource.publisher.string != filemetadata['printSource_pu']:
-        printSource.publisher.string = filemetadata['printSource_pubPlace']
-    """
     ## printSource publisher
     if re.search(",", filemetadata['printSource_publisher']):
         meta_all_publisher = filemetadata['printSource_publisher'].split(",")
@@ -382,19 +303,14 @@ def compare_printSource(filemetadata, xml_file):
 
     all_publisher = printSource.find_all("publisher")
     for ind, pub in enumerate(meta_all_publisher):
-        print("pub counter: ",pub, counter)
-        print(printSource)
         try:
             if all_publisher[ind].string != pub:
-                print(all_publisher, pub)
                 all_publisher[ind].string = pub
             printSource.insert(counter+1, all_publisher[ind])
-            print(printSource)
             counter += 1
         except IndexError:
             new_tag = xml_file.new_tag("publisher")
             new_tag.string = str(pub)
-            print(new_tag)
             printSource.insert(counter+1, new_tag)
             counter += 1
 
